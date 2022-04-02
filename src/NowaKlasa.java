2563b179fcc47209ed1bb9581f75ec8c38c8687b
Paweł Recław <preclaw@purestorage.com>
2022-04-02 12:24:25 +0200

    Drugi wpis w historii plików

---
+++ src/NowaKlasa.java
@@ -0,0 +1,18 @@
+/* komentarz wieloliniowy
+tutaj mogę tworzyć
+wiele
+linii komentarza
+ */
+
+/**
+ * Ta klasa jest moją pierwszą klasą!
+ *
+ * Ten komentarz jest częścią dokumentacji!
+ */
+public class NowaKlasa {
+    // Do zrobienia
+    // TODO: musze pamietac zeby kupic mleko
+    // fixme: koło odpadło z samochodu
+
+    // Kod powinien się sam dokumentować
+}
